15114d2c8d1f559b9fe262c298f8949a3d2c357a
Guarp <123624288+Guarp@users.noreply.github.com>
2023-09-04 22:07:37 +0800

    Add files via upload

--- goodthing/hello.js
+++ goodthing/hello.js
@@ -1,14 +1,19 @@
 function check() {
     let pwdg=document.getElementById("pwd").value;
+    let myElement = document.getElementById("myDiv");
     // console.log(pwdg);
     if(pwdg == 114514){
-        alert("密码正确！！！");
+        alert("6");
+        myElement.innerHTML = ";)";
+        alert("6");
         window.location = "./6.html"
     }else if(pwdg.length == 0){
         document.getElementById("errpwd").style.display = "inline";
         document.getElementById("errpwd2").style.display = "none";
+        myElement.innerHTML = ":/";
     }else{
         document.getElementById("errpwd").style.display = "none";
         document.getElementById("errpwd2").style.display = "inline";
+        myElement.innerHTML = ":(";
     }
 }
